1de80e86f5d0dadc9e14f7fb05c803e787a6f547
최예인 <yichoi97dev@gmail.com>
2021-03-19 00:00:26 +0900

    커서 애니메이션 추가

--- pf-dev/src/components/Home.js
+++ pf-dev/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './../animation.css';
 
 const Home = () => {
   return (
@@ -6,11 +7,15 @@ const Home = () => {
         <div className="content-area">
         <pre>{`<body>`}</pre>
         <pre>{`<h>`}</pre>
-        <h1 className="home-content">안녕하세요.<br/>
-            <span className="red">그림쟁이</span>
-            <span className="mint">Web developer1</span><br/>
-            <span className="blue">최예인</span>
-        입니다.</h1>
+        <div class="typewriter">   
+          <h1 className="home-content">
+            <span className="active">안녕하세요.</span><br/>
+              <span className="red">그림쟁이</span>
+              <span className="mint">Web developer1</span><br/>
+              <span className="blue">최예인</span>
+              입니다.
+          </h1>
+        </div>
         <pre>{`</h>`}</pre>
         <pre>{`</body>`}</pre>
         </div>
